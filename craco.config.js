afc06764e1260c5f31e231c16c94a855b98e7a54
Renato <tato_ap@hotmail.com>
2022-08-09 14:56:47 -0300

    Deploy da aplicação

--- craco.config.js
+++ craco.config.js
@@ -13,4 +13,20 @@ module.exports = {
       },
     },
   ],
+  reactScriptsVersion: 'react-scripts' /* (default value) */,
+  webpack: {
+    mode: 'extends',
+    configure: {
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            enforce: 'pre',
+            use: ['source-map-loader'],
+          },
+        ],
+      },
+      ignoreWarnings: [/Failed to parse source map/],
+    },
+  },
 };
